40fa01d07d2db6439b1a55535747c41469d8ba0d
KevinDGnanih <kevin.gnanih@gmail.com>
2022-06-09 22:31:44 +0000

    added notification Css, shopping bag preview, additiona messages and fixed typo

--- bag/contexts.py
+++ bag/contexts.py
@@ -32,14 +32,12 @@ def bag_contents(request):
                     'size': size,
                 })
 
-
-
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-        free_delivery_delat = settings.FREE_DELIVERY_THRESHOLD - total
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
-        free_delivery_delat = 0
+        free_delivery_delta = 0
 
     grand_total = delivery + total
 
@@ -48,7 +46,7 @@ def bag_contents(request):
         'total': total,
         'product_count': product_count,
         'delivery': delivery,
-        'free_delivery_delat': free_delivery_delat,
+        'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
 
