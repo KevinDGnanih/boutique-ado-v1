03e87c84074c368e4b877a52a5d699a294045802
KevinDGnanih <kevin.gnanih@gmail.com>
2022-07-06 21:37:14 +0000

    latest version

--- profiles/views.py
+++ profiles/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-
 from .models import UserProfile
 from .forms import UserProfileForm
 
 from checkout.models import Order
 
+
 @login_required
 def profile(request):
-    """
-    Display the user's profile
-    """
+    """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
@@ -23,7 +21,6 @@ def profile(request):
             messages.error(request, 'Update failed. Please ensure the form is valid.')
     else:
         form = UserProfileForm(instance=profile)
-    
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
@@ -40,7 +37,7 @@ def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
     messages.info(request, (
-        f'This is a past confirmation for order number {order_number}.'
+        f'This is a past confirmation for order number {order_number}. '
         'A confirmation email was sent on the order date.'
     ))
 
